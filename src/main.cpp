cd91cba26baed4754faa99f14de4a0f7349b98c2
fexsec <165517769+fexsecc@users.noreply.github.com>
2025-03-30 12:33:25 +0300

    readline support

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
-#include "lua.h"
+#include <stdio.h>
+#include <stdlib.h>
+extern "C" {
+    #include <lauxlib.h>
+    #include <lua.h>
+    #include <lualib.h>
+}
+#include <readline/readline.h> // autocompletion
+#include <readline/history.h>  // cmd history
 
 int main (int argc, char *argv[]) {
-    std::cout << "Hello world";
+    using_history();
+
+    char *line;
+    int error;
+    lua_State *L = luaL_newstate(); // opens lua
+    luaL_openlibs(L); // opens std libraries
+
+    while((line = readline("> ")) != NULL) {
+        add_history(line);
+        error = luaL_loadstring(L, line) || lua_pcall(L, 0, 0, 0);
+        if (error) {
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1); // pop error message from stack
+        }
+        free(line);
+    }
+    clear_history();
+
+    lua_close(L);
     return 0;
 }
